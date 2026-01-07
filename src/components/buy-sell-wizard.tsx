5e0bb7d3cf3f65d4880e4d4fa8b5b90445f39ec3
AdebestAA <aadebesta@gmail.com>
2026-01-06 19:32:12 -0800

    sell component

--- src/components/buy-sell-wizard.tsx
+++ src/components/buy-sell-wizard.tsx
@@ -1,12 +1,35 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Button } from "./ui/button";
+import Sell from "./buy-and-sell-components/sell";
+import Buy from "./buy-and-sell-components/buy";
 
 const BuySellWizard = () => {
+  const [actionBtn, setActionBtn] = useState<"Sell" | "Buy">("Sell");
+
   return (
-    <div className='h-full w-full rounded-lg flex items-center justify-center '>
+    <div className="h-full w-full rounded-lg ">
+      {/* <h1>Buy Sell Wizard</h1> */}
 
-        <h1>Buy Sell Wizard</h1>
+      <section>
+        <header className="flex justify-center">
+          <Button
+            onClick={() => setActionBtn("Buy")}
+            className="cursor-pointer"
+          >
+            Buy
+          </Button>
+          <Button
+            onClick={() => setActionBtn("Sell")}
+            className="cursor-pointer"
+          >
+            Sell
+          </Button>
+        </header>
+
+        {actionBtn == "Sell" ? <Sell /> : <Buy />}
+      </section>
     </div>
-  )
-}
+  );
+};
 
-export default BuySellWizard
+export default BuySellWizard;
---
+++ src/utils/buy-and-sell-utils.ts
@@ -0,0 +1,15 @@
+export const currencyEnum: string[] = ["Dollars", "Pounds", "Euros", "Yen"];
+
+export const bullionData: string[] = [
+  "Gold",
+  "Silver",
+  "Platinum",
+  "Palladium",
+];
+
+export const durationEnum: string[] = [
+  "Permanent",
+  "Until Date",
+  "Immediately",
+  "Kill or Fill",
+];
