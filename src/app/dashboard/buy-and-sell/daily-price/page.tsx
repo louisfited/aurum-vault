498d0f5c00cbb30f15ed3b9a635eb40238895d23
AdebestAA <aadebesta@gmail.com>
2026-01-09 07:08:24 -0800

    daily price

--- src/app/dashboard/buy-and-sell/daily-price/page.tsx
+++ src/app/dashboard/buy-and-sell/daily-price/page.tsx
@@ -1,8 +1,30 @@
 "use client";
 
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { BadgeQuestionMark } from "lucide-react";
 import Link from "next/link";
+import { useForm } from "react-hook-form";
 import { FaExclamation } from "react-icons/fa6";
+import { z } from "zod";
+import { currencyEnum } from "@/utils/buy-and-sell-utils";
+import { Button } from "@/components/ui/button";
 
 const silverData = [
   {
@@ -34,10 +56,31 @@ const currencyData = [
   },
 ];
 
-const headingClass = "py-3 bg-card px-2";
+const formSchema = z.object({
+  option: z.enum(["buy", "sell"], { message: "Field Cant be empty" }),
+  typeOfGold: z.string({ message: `Field Cant be empty` }),
+  amount: z
+    .number({ message: "Amount is required" })
+    .min(1, "Amount must be at least 1"),
+  currency: z.string({ message: `Field Cant be empty` }),
+});
 const Page = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      option: "buy",
+      typeOfGold: "",
+      amount: 0,
+      currency: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
   return (
-    <div>
+    <div className="py-8">
       {/* Account Info */}
       <section className=" pt-2  rounded-lg bg-card">
         <div className="flex justify-between text-xl font-semibold py-3 px-4">
@@ -82,6 +125,155 @@ const Page = () => {
           </div>
 
           {/* Form */}
+          {/* form  */}
+          <section className="my-8">
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="space-y-8"
+              >
+                {/* option */}
+                <FormField
+                  control={form.control}
+                  name="option"
+                  render={({ field }) => (
+                    <FormItem className="">
+                      <FormLabel>I want to </FormLabel>
+
+                      <div className="w-full">
+                        <FormControl>
+                          <Select
+                            onValueChange={field.onChange}
+                            defaultValue={field.value}
+                          >
+                            <SelectTrigger className="w-full">
+                              <SelectValue placeholder="currency" />
+                            </SelectTrigger>
+
+                            <SelectContent>
+                              <SelectItem value="buy">buy</SelectItem>
+                              <SelectItem value="sell">sell</SelectItem>
+                            </SelectContent>
+                          </Select>
+                        </FormControl>
+
+                        <FormMessage />
+                      </div>
+                    </FormItem>
+                  )}
+                />
+
+                {/* type of gold */}
+
+                {/* type of gold */}
+                <FormField
+                  control={form.control}
+                  name="typeOfGold"
+                  render={({ field }) => (
+                    <FormItem className="">
+                      {/* <FormLabel>Currency</FormLabel> */}
+
+                      <div className="w-full">
+                        <FormControl>
+                          <Select
+                            onValueChange={field.onChange}
+                            defaultValue={field.value}
+                          >
+                            <SelectTrigger className="w-full">
+                              <SelectValue placeholder="currency" />
+                            </SelectTrigger>
+
+                            <SelectContent>
+                              <SelectItem value="buy">
+                                Gold In Londom
+                              </SelectItem>
+                              <SelectItem value="sell">
+                                Gold In Zurich
+                              </SelectItem>
+                              <SelectItem value="sell">
+                                Silver In Londom
+                              </SelectItem>
+                            </SelectContent>
+                          </Select>
+                        </FormControl>
+
+                        <FormMessage />
+                      </div>
+                    </FormItem>
+                  )}
+                />
+
+                <FormLabel>I want to spend up to</FormLabel>
+
+                <FormField
+                  control={form.control}
+                  name="amount"
+                  render={({ field }) => (
+                    <FormItem className="">
+                      {/* <FormLabel>Currency</FormLabel> */}
+
+                      <div className="w-full">
+                        <FormControl>
+                          <Input
+                            type="number"
+                            placeholder="Amount"
+                            {...field}
+                          />
+                        </FormControl>
+
+                        <FormMessage />
+                      </div>
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="currency"
+                  render={({ field }) => (
+                    <FormItem className="">
+                      {/* <FormLabel>Currency</FormLabel> */}
+                      <div className="w-full">
+                        <FormControl>
+                          <Select>
+                            <SelectTrigger className="w-full">
+                              <SelectValue placeholder="currency" />
+                            </SelectTrigger>
+                            <SelectContent>
+                              {currencyEnum.map((item) => (
+                                <SelectItem key={item} value={item}>
+                                  {item}
+                                </SelectItem>
+                              ))}
+                            </SelectContent>
+                          </Select>
+                        </FormControl>
+                        <FormMessage />
+                      </div>
+                    </FormItem>
+                  )}
+                />
+
+                <div>
+                  <Button
+                    type="button"
+                    className="rounded-full w-full bg-yellow-500"
+                  >
+                    Buy
+                  </Button>
+                </div>
+              </form>
+            </Form>
+          </section>
+
+          <footer className="dark:bg-gray-600 bg-gray-200 p-4 rounded-b-lg">
+            <p>
+              We charge 0.5% for buying or selling gold, silver, platinum or
+              palladium at the Daily Price. There is an additional 0.3%
+              currency-switching fee for Daily Price orders set in British
+              Pounds or in Euros.
+            </p>
+          </footer>
         </article>
 
         {/* buy sell wizard */}
@@ -102,7 +294,6 @@ const Page = () => {
           </div>
         </>
       </div>
-      {/* </DashboardContainer> */}
     </div>
   );
 };
